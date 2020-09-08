e31bda31c096bf429612ad6203e9fe9eb1b2bd94
kblaze1996 <52061370+kblaze1996@users.noreply.github.com>
2020-09-07 21:36:30 -0700

    Resolved map rendering issues with react component

--- public/javascripts/esrimap.js
+++ public/javascripts/esrimap.js
@@ -28,7 +28,7 @@ $(document).ready(() => {
           weight: "bold"
         }
       },
-      labelPlacement: "above-center",
+      // labelPlacement: "above-center",
       labelExpressionInfo: {
         expression: "$feature.IncidentName"
       }
@@ -37,10 +37,28 @@ $(document).ready(() => {
     // adds fire names to fireLayer
     fireLayer.labelingInfo = [fireLabelClass];
 
-    // Incidents
+    /*
+      INCIDENTS
+      ---------------------------------
+      Useful info in layer: 
+      - properties."DailyAcres": 11.1
+      - properties."DiscoveryAcres": 0.1
+      - properties."FireCause": "Human"
+      - properties."FireCauseGeneral": "Utilities"
+      - properties."FireCauseSpecific": "Power Generation/Transmission"
+      - properties."ModifiedOnDateTime": 1590524946247.0 
+          - In Epoch time format: https://www.epochconverter.com/
+      - properties."POOCounty": "San Bernardino"
+      - properties."FireBehaviorGeneral"
+          - FireBehaviorGeneral1 2 3 and 4
+      - properties."PredominantFuelGroup": "Grass-Shrub"  
+      - properties."PredominantFuelModel": "GS2" 
+          - Codes for Fuel Models found here: https://www.fs.fed.us/rm/pubs/rmrs_gtr153.pdf
+          - It would be good to add a description to these codes somewhere for context
+    */
     var incidentLayer = new GeoJSONLayer({
       // url: "https://opendata.arcgis.com/datasets/68637d248eb24d0d853342cba02d4af7_0.geojson"
-      url: "data/incidents.geojson",
+      url: "data/incidents.geojson", // locally stored instead
       renderer: {
         type: "simple",
         symbol: {
@@ -67,7 +85,7 @@ $(document).ready(() => {
           weight: "bold"
         }
       },
-      labelPlacement: "above-center",
+      // labelPlacement: "above-center",
       labelExpressionInfo: {
         expression: "$feature.IncidentName"
       }
@@ -102,6 +120,18 @@ $(document).ready(() => {
     });
 
     view.ui.add(scalebar, "bottom-left");
+
+    // possible events: pointer-move, click
+    view.on("click", function(event) {
+      view.hitTest(event).then(function(response) {
+        console.log(response);
+        // check if a feature is returned from the layer 
+        // do something with the result graphic
+        const graphic = response.results.filter(function(result) {
+          console.log(result);
+        })
+      })
+    })
 
     $('#fireLayerCheck').on('change', function () {
       fireLayer.visible = !fireLayer.visible;
