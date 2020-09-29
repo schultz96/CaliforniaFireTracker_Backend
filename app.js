981ca970e7e8bf622cb67c983a8d500fb5bc4f48
kblaze1996 <52061370+kblaze1996@users.noreply.github.com>
2020-09-29 16:34:21 -0700

    catches bad urls with express

--- app.js
+++ app.js
@@ -12,13 +12,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
 app.use(express.static(__dirname + '/public'))
 
 app.use('/api', apiRouter);
+
+// catches bad URLs
+app.use('*', express.static(path.join(__dirname, 'public/index.html')));
 
 module.exports = app;
